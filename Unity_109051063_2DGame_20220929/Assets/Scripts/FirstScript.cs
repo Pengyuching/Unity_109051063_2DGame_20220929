91c65b50dffe9369bd55d507d97dc0aca47d1db7
Pengyuching <clairecos5487@gmail.com>
2022-11-07 16:03:46 +0800

    kaeru

---
+++ Unity_109051063_2DGame_20220929/Assets/Scripts/FirstScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+namespace Kaeru
+{
+    /// <summary>
+    /// 第一個程式
+    /// </summary>
+    public class FirstScript : MonoBehaviour
+    {
+        #region 註解區域
+        // 縮排 快捷鍵 Tab
+        // 格式化文件 Ctrl + K D
+        // 單行註解
+
+        /*
+         * 多行註解
+         * 多行註解
+         */
+        #endregion
+
+        #region 事件區域
+        //喚醒事件：遊戲開始時執行一次
+        private void Awake()
+        {
+            //輸出(訊息);
+            print("哈囉，沃德:D");
+        }
+
+        //開始事件：在 Awake 後執行一次
+        private void Start()
+        {
+            print("<color=lime>這是開始事件</color>");
+        }
+
+        //更新事件：在 Start 後執行，執行次數約 60 FPS
+        private void Update()
+        {
+            print("<color=red>更新事件！</color>");
+        }
+        #endregion
+    }
+}
