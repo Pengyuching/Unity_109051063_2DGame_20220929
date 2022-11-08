ed750ce9567577e1bb791750e79c1eba6d9822fa
Pengyuching <clairecos5487@gmail.com>
2022-11-08 22:10:44 +0800

    finish

---
+++ Unity_109051063_2DGame_20220929/Assets/Scripts/PlayerFireSystem.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Kaeru
+{
+    /// <summary>
+    /// 玩家發射
+    /// </summary>
+    public class PlayerFireSystem : FireSystemBace
+    {
+        // 偵測玩家的輸入行為：鍵盤、滑鼠、觸控、搖桿
+        private void Update()
+        {
+            // 如果 玩家 按下空白鍵 就生成子彈
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                SpawnBullet();
+            }
+        }
+    }
+}
+
